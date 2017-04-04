a750e295583d336807b014db942dfd3460549d22
jpiovar <jpiovar@sk.ibm.com>
2017-04-04 19:14:47 +0200

    config variable import export to component

--- start/app/ts/app.component.ts
+++ start/app/ts/app.component.ts
@@ -1,11 +1,15 @@
 import {Component} from 'angular2/core';
+import {Config} from './config.service';
 
 @Component({
     selector: 'component-basic-1', 
-    //template: '<h1>bbbb111</h1>'   
+    //template: '<h1>bbbb111 {{ okvariable }}</h1>'   
     //moduleId: module.id,
     templateUrl: 'app/views/app.component.html',
     styleUrls: ['app/css/app.component.css']
 })
 
-export class AppComponent {}
+export class AppComponent {
+  //okvariable = 'ok variable value';
+  okvariable = Config.MAIN_HEADING;
+}
---
+++ start/app/ts/config.service.ts
@@ -0,0 +1,3 @@
+export class Config{
+  static MAIN_HEADING: string = "ok content from config";
+}
