f6dec9d1e99327528eda3bf0b0fc1145b08cd892
jpiovar <jpiovar@sk.ibm.com>
2017-04-04 17:56:23 +0200

    start component

---
+++ hello/app/hello_world_app.component.ts
@@ -0,0 +1,11 @@
+@Component({
+   selector: 'my-app'
+})
+
+@View({
+  template: '<h2>Hello World !!</h2>'
+})
+
+export class MyHelloWorldClass {
+
+}
---
+++ start/app/ts/app.component.ts
@@ -0,0 +1,11 @@
+import {Component} from 'angular2/core';
+
+@Component({
+    selector: 'component-basic-1', 
+    //template: '<h1>bbbb111</h1>'   
+    //moduleId: module.id,
+    templateUrl: 'app/views/app.component.html',
+    styleUrls: ['app/css/app.component.css']
+})
+
+export class AppComponent {}
