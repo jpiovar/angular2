c61069bf088ccd74f2df2873ab9a000e4f133369
jpiovar <jpiovar@sk.ibm.com>
2017-04-06 19:17:16 +0200

    no message

--- mean2/routes/tasks.js
+++ mean2/routes/tasks.js
@@ -62,4 +62,77 @@ router.get('/task/:id', function(req, res, next){
 });
 
 
+var getMaxObjArray = function(objArr) {
+  var maxObjArr = 0;
+  for(var i in objArr){
+    if(objArr[i].id > maxObjArr){
+      maxObjArr = objArr[i].id;
+    }
+  }
+  return maxObjArr;
+}
+
+
+// save task
+router.post('/task', function(req, res, next){
+  var task = req.body;
+  if(!task){
+    res.status(400);
+    res.json({
+      "error": "Bad data"
+    });
+  } else {
+    MongoClient.connect("mongodb://localhost:27017/mean", function (err, db) {
+
+	     if(err){
+         reject(err);
+         return;
+       }
+
+
+       db.collection('Record', function (err, collection) {
+
+
+          collection.find().toArray(function(err, items) {
+
+            if(err) {
+              reject(err); return;
+            }
+
+            console.log('service items from mongodb '+items);
+            console.log('pr next'+items);
+
+
+            var maxRecord = getMaxObjArray(items);
+            console.log(maxRecord);
+
+            collection.insert({ id: maxRecord+1, item: record }, function(err, result) {
+
+              if(err) {
+                reject(err);
+                return;
+              }
+
+              db.close();
+              //resolve(result);
+
+  	        });
+
+          });
+
+
+
+	     });
+
+
+
+
+
+
+
+
+    });
+  }
+});
+
 module.exports = router;
