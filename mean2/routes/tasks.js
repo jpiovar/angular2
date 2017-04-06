c95c71535004152ef856201c98dc537ae8f50ec0
jpiovar <jpiovar@sk.ibm.com>
2017-04-06 12:31:36 +0200

    mongo client get items  get  specific item by id

--- mean2/routes/tasks.js
+++ mean2/routes/tasks.js
@@ -1,8 +1,63 @@
 var express = require('express');
 var router = express.Router();
 
+var MongoClient = require('mongodb').MongoClient;
+var colectionMongoDb = 'tasks';
+
+// get all tasks
 router.get('/tasks', function(req, res, next){
-    res.send('TASK API');
+    //res.send('TASK API');
+    MongoClient.connect("mongodb://localhost:27017/mean", function (err, db) {
+
+      if(err) {reject(err); return;}
+
+      db.collection('Record', function (err, collection) {
+
+        collection.find().toArray(function(err, items) {
+
+          if(err) {
+            reject(err); return;
+          }
+
+          console.log('service items from mongodb '+items);
+          res.json(items);
+          db.close();
+
+        });
+
+      });
+
+    });
+
 });
+
+
+// get single task
+router.get('/task/:id', function(req, res, next){
+    //res.send('TASK API');
+    MongoClient.connect("mongodb://localhost:27017/mean", function (err, db) {
+
+      if(err) {reject(err); return;}
+
+      db.collection('Record', function (err, collection) {
+
+        collection.findOne({id:2}, function(err, item) {
+
+          if(err) {
+            reject(err); return;
+          }
+
+          console.log('service items from mongodb '+item);
+          res.json(item);
+          db.close();
+
+        });
+
+      });
+
+    });
+
+});
+
 
 module.exports = router;
